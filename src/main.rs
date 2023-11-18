dd3986fd685d418e1144c74d9b747a272b9dbcbb
numinnex <g.koszyk98@gmail.com>
2023-11-18 12:57:09 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use persist::persister::{FilePersister, Persister};
+
+
 mod persist;
 mod utils;
 
-fn main() {
-    println!("Hello, world!");
+#[monoio::main(driver = "fusion")]
+async fn main() {
+    let persister = FilePersister {};
+    let bytes = b"asdf".to_vec();
+    let result = persister.save("foo.txt", bytes).await;
+    if let Err(err) = result {
+        println!("Error: {:?}", err);
+    }
 }
--- src/persist/persister.rs
+++ src/persist/persister.rs
@@ -1,12 +1,21 @@
-use std::io::Error;
+use std::io::{Error, BufRead};
 use crate::utils::file;
+use monoio::io::{BufWriter, BufReader};
 
+pub trait Persister {
+    async fn save(&self, path: &str, bytes: Vec<u8>) -> Result<(), Error>;
+    async fn load(&self, path: &str) -> Result<String, Error>;
+}
 
 pub struct FilePersister {}
-impl FilePersister {
+
+impl Persister for FilePersister {
     async fn save(&self, path: &str, bytes: Vec<u8>) -> Result<(), Error> {
-        let mut file = file::create_file(path).await?;
-        file.write_all_at(bytes, 0).await.0.map_err(|e| e.into())
+        let file = file::create_file(path).await?;
+        let writer = BufWriter::new(file);
+        let xd = writer.into_inner();
+        let res = xd.write_at(bytes,0).await;
+        Ok(())
     }
 
     async fn load(&self, path: &str) -> Result<String, Error> {
